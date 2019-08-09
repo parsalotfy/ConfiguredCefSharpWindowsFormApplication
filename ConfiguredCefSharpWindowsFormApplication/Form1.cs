bc546102b886d2600bbc9371e7e0a130a1ffa67a
parsalotfy <parsalotfy@outlook.com>
2019-08-09 20:24:46 +0430

    Add some bullshit to Form1.cs

--- ConfiguredCefSharpWindowsFormApplication/Form1.cs
+++ ConfiguredCefSharpWindowsFormApplication/Form1.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
 
 namespace ConfiguredCefSharpWindowsFormApplication
 {
@@ -15,6 +9,27 @@ namespace ConfiguredCefSharpWindowsFormApplication
         public Form1()
         {
             InitializeComponent();
+            // Start the browser after initialize global component
+            InitializeChromium();
+        }
+
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
         }
     }
 }
